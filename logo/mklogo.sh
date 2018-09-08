67ee86c43592bc56489c43aacb604a5e43901ea7
Jerry <Jerry981028@gmail.com>
2018-09-08 17:26:58 +0800

    Update mklogo.sh

--- logo/mklogo.sh
+++ logo/mklogo.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 ffmpeg -i "$1" -pix_fmt rgb565 -s 1920x1080 output.bmp
+[ $? != 0 ] && echo "ffmpeg exited with a non-zero value" && read -p 'Continue?'
 cat logo.img.1 output.bmp logo.img.3 logo.img.4 > logo.img
-stat logo.img logo.img.orig
+[ "$(du -b logo.img |awk '{print $1}')" == "$(du -b logo.img.orig |awk '{print $1}')" ] && echo -e "\nOK\n" || echo -e "\nWarning: Wrong size! Don't flash\n"
+md5sum logo.img
