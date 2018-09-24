b4eb5f238f38b369ea38df618c13c66c9f431a39
Jerry <Jerry981028@gmail.com>
2018-09-24 20:04:57 +0800

    add android offset tool

--- offset-android.sh
+++ offset-android.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/system/bin/sh
 EMMC="/dev/mmcblk1"
 
 _bootloader="bootloader 0x000000000000 0x000000400000"
@@ -28,17 +28,23 @@ print(){
 }
 setup(){
     [ -e $1 ] && die "Error: file ${1} exist"
-    loop_device=$(sudo losetup --show -f "$EMMC" --offset "$(($2))" --sizelimit "$(($3))")
-    echo "$loop_device" |grep -Eq '/dev/.*loop[0-9]+' && \
-        ln -s "$loop_device" "$1" || \
+    loop_device=$(losetup --show -f "$EMMC" --offset "$(($2))" --sizelimit "$(($3))")
+    if echo "$loop_device" |grep -Eq '/dev/.*loop[0-9]+'
+    then
+        ln -s "$loop_device" "$1"
+    else
         die "Error: unable to assign loop device"
+    fi
 }
 detach(){
     loop_device=$(readlink "$1")
-    echo "$loop_device" |grep -Eq '/dev/.*loop[0-9]+' && \
-        sudo losetup -d "$loop_device" || \
+    if echo "$loop_device" |grep -Eq '/dev/.*loop[0-9]+'
+    then
+        losetup -d "$loop_device"
+    else
         die "Error: cannot find loop device for ${1}"
-    sudo losetup "$loop_device" 2>/dev/null |grep -Fq "$EMMC" && die "Cannot detach now. Maybe the loop device is mounted."
+    fi
+    losetup "$loop_device" 2>/dev/null |grep -Fq "$EMMC" && die "Cannot detach now. Maybe the loop device is mounted."
     rm "$1"
 }
 check_partname(){
@@ -53,7 +59,7 @@ check_partname(){
     die "Error: partname is invaild"
 }
 
-[ -e $EMMC ] || die "Error: emmc not found"
+if ! [ -e $EMMC ] ; then die "Error: emmc not found" ; fi
 if [ "$1"  == "-d" ]; then
     check_partname "$2"
     detach "$2"
